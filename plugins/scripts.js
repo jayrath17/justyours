b3685ad04bf7c9ec02c995fe129f32d5789c16e4
Ronald Nolasco <alexnolasco_2010@hotmail.com>
2021-07-07 21:19:15 -0600

    fix(scripts.js): updated

--- plugins/scripts.js
+++ plugins/scripts.js
@@ -1,46 +1,47 @@
-// TODO: WE SHOULD ENABLE THIS ONLY ON PRODUCTION
-document.onkeydown = function (e) {
-  if (event.keyCode == 123) {
+if (process.env.NODE_ENV == 'production') {
+  document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+      return false
+    }
+    if (event.metaKey) {
+      // return false
+    }
+    if (
+      e.ctrlKey &&
+      e.shiftKey &&
+      (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'i'.charCodeAt(0))
+    ) {
+      return false
+    }
+    if (
+      e.ctrlKey &&
+      e.shiftKey &&
+      (e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'c'.charCodeAt(0))
+    ) {
+      return false
+    }
+    if (
+      e.ctrlKey &&
+      e.shiftKey &&
+      (e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'j'.charCodeAt(0))
+    ) {
+      return false
+    }
+    if (
+      e.ctrlKey &&
+      (e.keyCode == 'U'.charCodeAt(0) || e.keyCode == 'u'.charCodeAt(0))
+    ) {
+      return false
+    }
+    if (
+      e.ctrlKey &&
+      (e.keyCode == 'S'.charCodeAt(0) || e.keyCode == 's'.charCodeAt(0))
+    ) {
+      return false
+    }
+  }
+  document.addEventListener('contextmenu', function (e) {
+    e.preventDefault()
     return false
-  }
-  if (event.metaKey) {
-    // return false
-  }
-  if (
-    e.ctrlKey &&
-    e.shiftKey &&
-    (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'i'.charCodeAt(0))
-  ) {
-    return false
-  }
-  if (
-    e.ctrlKey &&
-    e.shiftKey &&
-    (e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'c'.charCodeAt(0))
-  ) {
-    return false
-  }
-  if (
-    e.ctrlKey &&
-    e.shiftKey &&
-    (e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'j'.charCodeAt(0))
-  ) {
-    return false
-  }
-  if (
-    e.ctrlKey &&
-    (e.keyCode == 'U'.charCodeAt(0) || e.keyCode == 'u'.charCodeAt(0))
-  ) {
-    return false
-  }
-  if (
-    e.ctrlKey &&
-    (e.keyCode == 'S'.charCodeAt(0) || e.keyCode == 's'.charCodeAt(0))
-  ) {
-    return false
-  }
+  })
 }
-document.addEventListener('contextmenu', function (e) {
-  e.preventDefault()
-  return false
-})
